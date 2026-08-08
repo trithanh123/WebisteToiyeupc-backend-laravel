94ba7abf7919171741efaaed32d6b7f132392c22
trithanh123 <phamtrithanh1234@gmail.com>
2026-08-08 14:47:45 +0700

    Chore: Remove temporary indexing route for security

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/run-qdrant-index', function () {
-    \Illuminate\Support\Facades\Artisan::call('qdrant:index');
-    return "<pre>" . \Illuminate\Support\Facades\Artisan::output() . "</pre>";
-});
