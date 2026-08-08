0fb812b0a62996ef5013a90beaa078b687949f98
trithanh123 <phamtrithanh1234@gmail.com>
2026-08-08 14:40:27 +0700

    Feat: Add HTTP route to trigger Qdrant indexing

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/run-qdrant-index', function () {
+    \Illuminate\Support\Facades\Artisan::call('qdrant:index');
+    return "<pre>" . \Illuminate\Support\Facades\Artisan::output() . "</pre>";
+});
