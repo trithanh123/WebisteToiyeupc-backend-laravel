72bcfe3ee891ed0902191c6a8623f77c38212780
trithanh123 <phamtrithanh1234@gmail.com>
2026-08-16 20:51:16 +0700

    fix debug

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,12 @@ Route::get('/run-qdrant-index', function () {
 // OAuth routes cần session để PKCE hoạt động
 Route::get('/api/auth/{provider}/redirect', [AuthController::class, 'redirectToProvider']);
 Route::get('/api/auth/{provider}/callback', [AuthController::class, 'handleProviderCallback']);
+
+// Temp route to read logs
+Route::get('/debug-logs', function () {
+    $logFile = storage_path('logs/laravel.log');
+    if (file_exists($logFile)) {
+        return "<pre>" . file_get_contents($logFile) . "</pre>";
+    }
+    return "No logs found.";
+});
